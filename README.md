436d41afc5085ef9778dac5d3a355ffff8c87ec0
jingxiao_yz <2437194031@qq.com>
2023-05-17 20:35:34 +0800

    tools文件夹备注

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # nodule-detect
+
 logs 和 model_data 文件夹内文件太大, 暂没有上传
+
+## 检测代码结构
+
+#### classification_utils
+
+#### logs
+
+#### model_data
+
+#### tools
+
+1. dic2img.py
+   dicom格式转图片格式（.png）
+2. json2execl.py
+   josn格式数据转excel表
+3. outline_back_mixed.py
+   边缘线贴回图片脚本，起勾勒结节外形的作用
+4. outline.py
+   画边缘线的脚本
+5. png2svg.py
+   图片转svg脚本，网站的前端页面支持svg格式的图片显示
+6. replace_path.py
+   路径替换函数
+
+#### unet_nets
+
+#### unet_utils
+
+#### yolox_nets
+
+#### yolox_utils
--- awake_manage.sh
+++ awake_manage.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 while true
-do	
-	# #重启时间周期
-	# sleeptime=4h
-	
+do
+
 	#先停止上次进程
 	pythonpath='/home/predict/manage.py'
 	PID=`ps -ef | grep $pythonpath | grep -v grep | awk '{print $2}'`
@@ -12,21 +10,21 @@ do
 	then
 	kill -9 $PID
 	fi
-	
+
 	#脚本启动当前时间
 	nowtime=$(date "+%Y-%m-%d %H:%M:%S")
 	echo "$nowtime: 已启动$pythonpath" >> /home/predict/awake_manage.log
 	#下次重启时间(明天凌晨3点)
 	restarttime="$(date -d tomorrow "+%Y-%m-%d") 03:00:00"
 	echo "$pythonpath 下次重启时间：${restarttime}" >> /home/predict/awake_manage.log
-	
+
 	#转为时间戳
 	nowtime_stamp=$(date --date="$nowtime" +%s)
 	restarttime_stamp=$(date --date="$restarttime" +%s)
 
 	#启动应用
 	nohup /root/anaconda3/bin/python /home/predict/manage.py >> /home/predict/manage.log &
-	
+
 	#获取将要休眠的时间(秒)
 	sleeptime=$((restarttime_stamp - nowtime_stamp))
 	#如果sleeptime小于或等于0
@@ -42,6 +40,5 @@ do
 done
 
 
-
-
+####    后台启动命令，重启日志输出到awake_manage.log里面
 ####    nohup /home/predict/awake_manage.sh >> /home/predict/awake_manage.log &
